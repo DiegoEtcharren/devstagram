9f77d2656a5ce929b9616ce56381c009cc2bd695
Diego Etcharren <diegoe96@gmail.com>
2025-10-23 19:52:55 -0600

    Drozone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import Dropzone from "dropzone";
+Dropzone.autoDiscover = false;
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage : "Arrastra tu Foto",
+    acceptedFiles : ".jpg, .png, .jpeg, .gif",
+    addRemoveLinks : true, 
+    dictRemoveFile : "Borrar Foto",
+    maxFiles : 1, 
+    uploadMultiple : false
+}
+);
+
+
+
+
---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+@section('titulo')
+    Crear un nuevo Post
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:items-center">
+        <div class="md:w-1/2 px-10">
+            <form id="dropzone" action="/imagenes" class="dropzone !border-dashed !border-2 !border-gray-400 w-full h-96 rounded flex flex-col justify-center items-center">
+
+            </form>
+        </div>
+        <div class="md:w-1/2 px-10  bg-white p-6 rounded-lg shadow-xl mt-10 md:mt-0">
+            <form action="{{ route('register')}}" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Titulo
+                    </label>
+                    <input
+                        id="titulo"
+                        name="titulo"
+                        type="text"
+                        placeholder="Titulo de la Publicación"
+                        class="border-2 p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                        value="{{ old('titulo') }}"  
+                    >
+                    @error('titulo')
+                        <p class="bg-red-500 font-bold text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea
+                        id="descripcion"
+                        name="descripcion"
+                        placeholder="Descripción de la Publicación"
+                        class="border-2 p-3 w-full rounded-lg @error('name') border-red-500 @enderror" 
+                    >{{old('name')}}</textarea>
+                    @error('descripcion')
+                        <p class="bg-red-500 font-bold text-white my-2 rounded-lg text-sm p-2 text-center">{{$message}}</p>
+                    @enderror
+                </div>
+                <input 
+                type="submit"
+                value="Crear Publicación"
+                class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
+                uppercase font-bold w-full p-3 text-white rounded-lg"
+            >
+            </form>
+        </div>
+    </div>
+@endsection 
