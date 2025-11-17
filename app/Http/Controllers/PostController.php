172ddbaa780f0d250004d5f3c06154c080d2cf08
Diego Etcharren <diegoe96@gmail.com>
2025-11-17 16:43:17 -0600

    Comments Section

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except(['show', 'index']);
     }
 
     public function index(User $user) {
@@ -54,7 +54,10 @@ class PostController extends Controller
         return redirect()->route('posts.index', auth()->user()->username);
     }
 
-    public function show() {
-        return view('posts.show');
+    public function show(User $user, Post $post) {
+        return view('posts.show', [
+            'post' => $post, 
+            'user' => $user
+        ]);
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -64,7 +64,7 @@
                 value="Crear Publicación"
                 class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer
                 uppercase font-bold w-full p-3 text-white rounded-lg"
-            >
+                >
             </form>
         </div>
     </div>
